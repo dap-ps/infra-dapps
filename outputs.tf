ad87eca515801b03237515ceb96c91c01e6a02ee
Jakub Sokołowski <jakub@status.im>
2019-08-06 09:43:57 -0400

    comment out smtp outputs for now
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- outputs.tf
+++ outputs.tf
@@ -3,9 +3,9 @@
  * For details see: https://www.terraform.io/docs/providers/aws/r/iam_access_key.html
  **/
 
-output "smtp_username" {
-  value = "${aws_iam_access_key.smtp.id}"
-}
-output "smtp_password" {
-  value = "${aws_iam_access_key.smtp.ses_smtp_password}"
-}
+//output "smtp_username" {
+//  value = "${aws_iam_access_key.smtp.id}"
+//}
+//output "smtp_password" {
+//  value = "${aws_iam_access_key.smtp.ses_smtp_password}"
+//}
